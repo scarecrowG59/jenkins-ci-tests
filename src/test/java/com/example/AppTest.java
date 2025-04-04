5ef7b078fb3be5f918cb11ae2f94920a767e8ae9
scarecrowG59 <zak.cooldybayev@gmail.com>
2025-04-04 05:08:29 +0000

    Deleted code part with second test

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -9,11 +9,5 @@ public class AppTest {
     public void testAdd() {
         assertEquals(2, App.add(1, 1));
     }
-
-    @Test
-    public void testMainMethod() {
-        // Это даст Jacoco возможность покрыть main()
-        App.main(new String[]{});
-    }
 }
 

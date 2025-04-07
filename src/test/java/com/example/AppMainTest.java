c2d9f191ca32bbedbf6fad222c1c1d9895911fc6
scarecrowG59 <zak.cooldybayev@gmail.com>
2025-04-07 08:23:35 +0000

    fixed some shit again

--- src/test/java/com/example/AppMainTest.java
+++ src/test/java/com/example/AppMainTest.java
@@ -1,12 +1,15 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 public class AppMainTest {
 
     @Test
     public void testMainMethod() {
-        App.main(new String[] {});
+        new SpringApplicationBuilder(App.class)
+            .properties("server.port=0")
+            .run();
     }
 }
 
